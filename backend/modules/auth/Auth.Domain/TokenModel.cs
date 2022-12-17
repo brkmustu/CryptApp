a89dffffce0d0cee1b89a5b6009704b10dacb4d1
Mehmet Burak Muştu <47041552+brkmustu@users.noreply.github.com>
2022-12-18 01:43:49 +0300

    Auth.Domain.Shared projesi çıkarıldı

---
+++ backend/modules/auth/Auth.Domain/TokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth;
+
+public  class TokenModel
+{
+    public int AppId { get; set; }
+    public string AppName { get; set; }
+    public string ApiKey { get; set; }
+}
---
+++ backend/modules/auth/Auth.Domain/TokenResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Auth;
+
+public class TokenResult
+{
+    public string Token { get; set; }
+    public DateTime Expiration { get; set; }
+}
